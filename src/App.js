7992c99ee23c0471075e446d34e613e38de8f1db
Samuel Muli <samuelmuli95@gmail.com>
2024-03-04 15:33:15 +0300

    Set up react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Routes, Route } from 'react-router-dom';
 import SignUp from './Components/SignUp';
+import SignIn from './Components/SignIn';
 
 function App() {
   return (
     <div className="App">
-      <SignUp />
+      <Routes>
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
     </div>
   );
 }
