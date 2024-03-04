57051a2ce7c8404a6d625b8a1ecdcf1a304e7f95
Samuel Muli <samuelmuli95@gmail.com>
2024-03-04 17:09:20 +0300

    Implement reset password functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route, redirect, Navigate } from 'react-router-dom';
 import SignUp from './Components/SignUp';
 import SignIn from './Components/SignIn';
 import IncidentReport from './Components/IncidentReport';
+import PasswordReset from './Components/PasswordReset';
 
 const PrivateRoute = ({ children }) => {
   const token = localStorage.getItem("token")
@@ -18,6 +19,7 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/incidentreport" element={<PrivateRoute><IncidentReport /></PrivateRoute>} />
+        <Route path="/passwordreset" element={<PasswordReset />} />
       </Routes>
     </div>
   );
--- src/Components/PasswordReset.js
+++ src/Components/PasswordReset.js
@@ -1,5 +1,84 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function PasswordReset() {
-    return <div>PasswordReset</div>;
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+        confirmPassword: ""
+    })
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (formData.password !== formData.confirmPassword) {
+            alert("passwords do not match")
+            return
+        }
+
+        try{
+            fetch("https://ajali-b.onrender.com/signup", {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    email: formData.email,
+                    password: formData.password
+                })
+            })
+            .then(res => res.json())
+            .then((data) => {
+                if(data) {
+                    console.log("password reset", data)
+                    navigate("/signin")
+                }
+                else {
+                    console.log("password reset failed", data)
+                }
+            })
+        }catch(err) {
+            console.log(err)
+        }
+    }
+
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    type="email"
+                    name="email"
+                    placeholder="Please enter your email"
+                    onChange={handleChange}
+                    value={formData.email}
+                />
+                <input 
+                    type="password"
+                    name="password"
+                    placeholder="Enter your password"
+                    onChange={handleChange}
+                    value={formData.password}
+                />
+                <input 
+                    type="password"
+                    name="confirmPassword"
+                    placeholder="Re-enter Password"
+                    onChange={handleChange}
+                    value={formData.confirmPassword}
+                />
+                <button type="submit">Reset Password</button>
+            </form>
+        </div>
+    )
 }
