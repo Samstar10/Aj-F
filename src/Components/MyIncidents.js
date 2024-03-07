257c3ff1faf26690339012bfe949008c0a4b6503
Samuel Muli <samuelmuli95@gmail.com>
2024-03-07 15:18:15 +0300

    modifications

--- src/Components/MyIncidents.js
+++ src/Components/MyIncidents.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Sidebar from "./Sidebar";
 
 export default function MyIncidents() {
     return (
         <div>
+            <Sidebar />
             <h1>My Incidents</h1>
         </div>
     );
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -11,7 +11,7 @@ export default function Sidebar() {
     }
 
     return (
-        <aside className=" bg-blue-500 h-screen w-1/5 fixed left-0 top-0 pt-10 pb-5 flex flex-col justify-between">
+        <aside className="hidden bg-blue-500 h-screen w-1/5 fixed left-0 top-0 pt-10 pb-5 md:flex flex-col justify-between">
             <div className="flex flex-col space-y-6">
                 <NavLink to="/incidentreport" activeClassName="active">New Incident</NavLink>
                 <NavLink to="/myincidents" activeClassName="active">My Incidents</NavLink>
