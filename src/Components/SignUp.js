63d0dd63b729d1471650b8695ee7d5e234c7e453
Samuel Muli <samuelmuli95@gmail.com>
2024-03-07 09:35:47 +0300

    Add styles to sign up component

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -61,41 +61,79 @@ export default function SignUp() {
         // })
     }
 
+    const navigateToSignIn = () => {
+        navigate("/signin")
+    }
+
     return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="username">Username</label>
-            <input
-                type="text"
-                placeholder="username"
-                name="username"
-                value={formData.username}
-                onChange={handleChange}
-            />
-            <label htmlFor="email">Email</label>
-            <input
-                type="email"
-                placeholder="email"
-                name="email"
-                value={formData.email}
-                onChange={handleChange}
-            />
-            <label htmlFor="password">Password</label>
-            <input
-                type="password"
-                placeholder="password"
-                name="password"
-                value={formData.password}
-                onChange={handleChange}
-            />
-            <label htmlFor="confirmPassword">Confirm Password</label>
-            <input 
-                type="password"
-                placeholder="Confirm password"
-                name="confirmPassword"
-                value={formData.confirmPassword}
-                onChange={handleChange}
-            />
-            <button type="submit">Sign Up</button>
-        </form>
+        <div className="p-5 w-screen h-screen flex justify-between">
+            <div className="background-img hidden h-full w-1/2 rounded-[29px] xl:flex justify-start items-end p-10">
+                <div >
+                    <h1 className="text-3xl font-bold text-[37px] text-left text-[#FFFFFF]">Ajali</h1>
+                    <p className="text-[#FFFFFF] text-[25px] mt-4 font-normal">SWIFT ALERTS, TIMELY ACTIONS</p>
+                </div>
+            </div>
+            <div className="w-full xl:w-1/2 h-full px-5 md:px-40 lg:px-40 xl:px-20">
+                <div className="text-right mb-9">
+                    <h4 className="font-semibold text-xl">Ajali</h4>
+                </div>
+                <div className="flex flex-col justify-center my-auto">
+                    <div>
+                        <p className="text-base font-normal">Welcome to Ajali</p>
+                    </div>
+                    <div className="mt-6 mb-9  mx-auto">
+                        <div className="flex justify-center items-center p-3 bg-[#E2E3E3] rounded-[33px]">
+                            <div className="bg-inherit px-4 xl:px-8 py-1 rounded-[33px]">
+                                <button onClick={navigateToSignIn} className="text-[#0A2FB6] hover:text-white font-normal xl:text-base text-xs">Login</button>
+                            </div>
+                            <div className="bg-[#0A2FB6] px-4 xl:px-8 py-1 rounded-[33px]">
+                                <button className="text-white font-normal text-xs xl:text-base">Register</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <form onSubmit={handleSubmit} className="flex flex-col">
+                            <label htmlFor="username" className="text-sm xl:text-base font-normal text-left mb-3">Username</label>
+                            <input
+                                type="text"
+                                placeholder="username"
+                                name="username"
+                                value={formData.username}
+                                onChange={handleChange}
+                                className="border-[#AFAFAF] border-[1px] rounded-[40px] py-2 px-5 text-sm focus:outline-none mb-4"
+                            />
+                            <label htmlFor="email" className="text-sm xl:text-base font-normal text-left mb-3">Email</label>
+                            <input
+                                type="email"
+                                placeholder="email"
+                                name="email"
+                                value={formData.email}
+                                onChange={handleChange}
+                                className="border-[#AFAFAF] border-[1px] rounded-[40px] py-2 px-5 text-sm focus:outline-none mb-4"
+                            />
+                            <label htmlFor="password" className="text-sm xl:text-base font-normal text-left mb-3">Password</label>
+                            <input
+                                type="password"
+                                placeholder="password"
+                                name="password"
+                                value={formData.password}
+                                onChange={handleChange}
+                                className="border-[#AFAFAF] border-[1px] rounded-[40px] py-2 px-5 text-sm focus:outline-none mb-4"
+                            />
+                            <label htmlFor="confirmPassword" className="text-sm xl:text-base font-normal text-left mb-3">Confirm Password</label>
+                            <input 
+                                type="password"
+                                placeholder="Confirm password"
+                                name="confirmPassword"
+                                value={formData.confirmPassword}
+                                onChange={handleChange}
+                                className="border-[#AFAFAF] border-[1px] rounded-[40px] py-2 px-5 text-sm focus:outline-none mb-20"
+                            />
+                            <button type="submit" className="text-right bg-[#0A2FB6] ml-auto text-white text-sm xl:text-base flex justify-center items-center py-3 w-1/2 rounded-[36px] hover:bg-[#5573de]">Sign Up</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
