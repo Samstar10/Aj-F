371d4ad2c9b9295d17eca4b7ee9bf94f7d0a2b96
Samuel Muli <samuelmuli95@gmail.com>
2024-03-08 09:20:21 +0300

    Add styles to aside

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -11,10 +11,10 @@ export default function Sidebar() {
     }
 
     return (
-        <aside className="hidden bg-blue-500 h-screen w-1/5 fixed left-0 top-0 pt-10 pb-5 md:flex flex-col justify-between">
+        <aside className="hidden bg-[#0A2FB6] h-screen w-1/5 fixed left-0 top-0 pt-10 pb-5 md:flex flex-col justify-between">
             <div className="flex flex-col space-y-6">
-                <NavLink to="/incidentreport" activeClassName="active">New Incident</NavLink>
-                <NavLink to="/myincidents" activeClassName="active">My Incidents</NavLink>
+                <NavLink to="/incidentreport" activeClassName="active" className="text-white">New Incident</NavLink>
+                <NavLink to="/myincidents" activeClassName="active" className="text-white">My Incidents</NavLink>
             </div>
             <div className="mt-auto">
                 <button onClick={handleLogout} className="bottom-0">Sign Out</button>
