eee59fb2eadb8f072da548c7b62c7901e8095eac
Samuel Muli <samuelmuli95@gmail.com>
2024-03-11 09:48:02 +0300

    Initialize incident report component

--- src/Components/IncidentReport.js
+++ src/Components/IncidentReport.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "./Sidebar";
+import { useNavigate } from "react-router-dom";
 
 export default function IncidentReport() {
+    const [formData, setFormData] = useState({
+        title: "",
+        description: "",
+        location: "",
+        latitude: "",
+        longitude: ""
+    })
+    const navigate = useNavigate();
+
     return (
         <div>
             <Sidebar />
